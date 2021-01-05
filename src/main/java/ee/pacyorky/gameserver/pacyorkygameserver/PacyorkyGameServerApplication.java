f9678f72a29fae0d3bcd7e3a8ee123507c56a4c7
dmitrii <dmitrii.golikov91@gmail.com>
2021-01-05 17:45:41 +0200

    inital commit

---
+++ src/main/java/ee/pacyorky/gameserver/pacyorkygameserver/PacyorkyGameServerApplication.java
@@ -0,0 +1,13 @@
+package ee.pacyorky.gameserver.pacyorkygameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacyorkyGameServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacyorkyGameServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/pacyorky/gameserver/pacyorkygameserver/PacyorkyGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.pacyorky.gameserver.pacyorkygameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacyorkyGameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
